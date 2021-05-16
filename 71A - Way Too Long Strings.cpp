da5aa9e450bdb2270bf4cad4dc3732f22986cf50
Firestar162 <82413905+Firestar162@users.noreply.github.com>
2021-05-16 18:22:50 +0100

    71A - Way Too Long Strings

--- 71A - Way Too Long Strings.cpp
+++ 71A - Way Too Long Strings.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 int main() {
-	int c;
+	int n; 	// n = number of words that are going to be input
 	
-	cin >> c;
+	cin >> n;  // read n
 	
-	string str;
+	string str;  // str = the word that is going to be input
 	
-	for (int b = 0; b < c; b++) {
-		cin >> str;
-		if (str.length() > 10) {
-			cout << str.at(0) << str.length() - 2 << str.back() << endl;
+	for (int i = 0; b < n; i++) {	// repeat the following n times
+		
+		cin >> str;	// read str
+		
+		if (str.length() > 10) {	// if the number of letters in str exceeds 10:
+			cout << str.at(0) << str.length() - 2 << str.back() << endl;	// print the first letter, then the number of letters between the first and last letters, then the last letter
 		}
-		else {
-			cout << str << endl;
+		
+		else {	
+			cout << str << endl;	// else simply print str, because the number of letters will be less than 10
 		}
 	}
   
