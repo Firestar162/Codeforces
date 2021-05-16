c31bb8723caa4235d9dabfc5f1fae1fc0b43a4a0
Firestar162 <82413905+Firestar162@users.noreply.github.com>
2021-05-16 18:21:51 +0100

    4A - Watermelon

--- 4A - Watermelon.cpp
+++ 4A - Watermelon.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main() {
-    int w; // w = weight of watermelon
+    int w;   // w = weight of watermelon
 
-    cin >> w; // read w
+    cin >> w;   // read w
 
     if (w == 2){
-    	cout << "NO" << endl; // if w is 2, print "no" (because the only way to split 2 is 1+1)
+    	cout << "NO" << endl;   // if w is 2, print "no" (because the only way to split 2 is 1+1)
     }
     else if (w%2==0){
-    	cout << "YES" << endl; // else if w is divisible by 2, print "yes" (because you can split any even number - except 2 - into 2 even integers)
+    	cout << "YES" << endl;  // else if w is divisible by 2, print "yes" (because you can split any even number - except 2 - into 2 even integers)
     }
     else {
-    	cout << "NO" << endl; // else print "no" (because the only other numbers are odd, and you can't split an odd number into 2 even integers)
+    	cout << "NO" << endl;   // else print "no" (because the only other numbers are odd, and you can't split an odd number into 2 even integers)
     }
     return 0;
 
